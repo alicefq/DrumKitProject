f2727d5da34390ef3b7c3f7efaa3fc33e503e581
Alice FQ <alice.fitzsimonsq@gmail.com>
2022-12-22 15:42:20 +0100

    adding audio to buttons
    experimenting with adding audio to buttons, and use of this

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var numOfButtons = document.querySelectorAll(".drum").length;
 //add event listener to all 7 buttons:
 for (var i = 0; i<numOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
+       
+        this.style.color = "white";
 });
 }
+
+
+ //var audio = new Audio("sounds/tom-1.mp3");
+        //audio.play();
